d7e5491a17ac6f0b350920b934bf92c43ba05373
Mila Roisin <mila.roisin@gmail.com>
2019-03-12 11:07:27 -0400

    Shiny template added for further implementation.

--- Shiny.R
+++ Shiny.R
@@ -1,1 +1,41 @@
-# Shiny Application will be implemented in here
+##Motor Trend Car Road Tests - Built in data set
+
+#Clearing the workspace before running the script in case program produces errors; NB. use browser() -- R will stop and turn the
+#console to user.
+
+rm(list=ls())
+library(shiny)
+
+#Building the UI Component for Shiny
+
+ui <- fluidPage("Hello World")
+# *input() functions,
+ =
+# *output() functions
+
+
+
+#Server instructions
+
+server <- function(input, output) {
+  
+}
+
+
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+my_data <- read.table("/Users/milaroisin/Documents/GitHub/plotShiny/mtcars.csv")
+head(mtcars)
+
+#structure of dataset
+str(mtcars)
+
+#Getting dimensions of the dataset -- 32 objects with 11 variables
+dim(mtcars)
+plot(density(mtcars$wt))
+summary(mtcars)
