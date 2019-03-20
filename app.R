135f8f49b1cae038c2aa9718c9b610f5f66b0e2b
Mila Roisin <mila.roisin@gmail.com>
2019-03-20 17:18:20 -0400

    Added data table render, theme and testing out theme selector to R Application.
    Signed-off-by: Mila Roisin <mila.roisin@gmail.com>

--- app.R
+++ app.R
@@ -1,35 +1,6 @@
 ##Motor Trend Car Road Tests - Built in data set
-
-#Clearing the workspace before running the script in case program produces errors; NB. use browser() -- R will stop and turn the
-#console to user.
-
-rm(list=ls())
-library(shiny)
-
-#Building the UI Component for Shiny
-
-ui <- fluidPage("Hello World")
-# *input() functions,
- =
-# *output() functions
-
-
-
-#Server instructions
-
-server <- function(input, output) {
-  
-}
-
-
-shinyApp(ui = ui, server = server)
-
-
-
-
-
-
-my_data <- read.table("/Users/milaroisin/Documents/GitHub/plotShiny/mtcars.csv")
+#Global Variables
+#my_data <- read.table("https://github.com/milaroisin/plotlyShiny/blob/d7e5491a17ac6f0b350920b934bf92c43ba05373/mtcars.csv")
 head(mtcars)
 
 #structure of dataset
@@ -39,3 +10,41 @@ str(mtcars)
 dim(mtcars)
 plot(density(mtcars$wt))
 summary(mtcars)
+
+#Clearing the workspace before running the script in case program produces errors; NB. use browser() -- R will stop and turn the
+#console to user.
+rm(list=ls())
+
+library(plotly)
+library(shiny)
+library(DT)
+library(shinythemes)
+
+#ui.R - Building the UI Component for Shiny
+# Observer Design Pattern - avoids code smells
+
+ui <- fluidPage(
+  shinythemes::themeSelector(),
+  theme=shinytheme("cyborg"),
+  titlePanel("Motor Trend Car Road Tests"),
+  DT::dataTableOutput("plotlytable"),
+  
+  sidebarLayout(
+    sidebarPanel(
+    textInput("txt", "Text input:", "text here"),
+    sliderInput("slider", "Slider input:", 1, 100, 30),
+    actionButton("action", "Button"),
+    actionButton("action2", "Button2", class = "btn-primary")
+  ),
+    mainPanel(plotOutput("plotlyShiny")
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$plotlytable = DT::renderDataTable({
+    mtcars
+  })
+}
+
+shinyApp(ui, server)
