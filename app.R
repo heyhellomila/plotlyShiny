d77582b56d292e8b838465c02a17934084ea9d91
Mila Roisin <mila.roisin@gmail.com>
2019-03-21 23:38:18 -0400

    Conditional Output added.

--- app.R
+++ app.R
@@ -24,23 +24,31 @@ library(shinythemes)
 # Observer Design Pattern - avoids code smells
 
 ui <- fluidPage(
-  shinythemes::themeSelector(),
-  theme=shinytheme("cyborg"),
-  titlePanel("Motor Trend Car Road Tests"),
+  theme="bootstrap.min.css", #external bootswatch theme outside of shinythemes package
+  titlePanel("Mila's First Tinkering with Shiny App for Motor Trend Car Road Tests"),
   DT::dataTableOutput("plotlytable"),
   
   sidebarLayout(
     sidebarPanel(
-    textInput("txt", "Text input:", "text here"),
-    sliderInput("slider", "Slider input:", 1, 100, 30),
-    actionButton("action", "Button"),
-    actionButton("action2", "Button2", class = "btn-primary")
-  ),
-    mainPanel(plotOutput("plotlyShiny")
+      
+      actionButton("Click", "Me!"),
+      
+      #Demonstrating Dropdown Menu
+      selectInput("select", label = h3("Memes that won the Internet's Heart?"), 
+                  choices = list("NyanCat" = 1, "Charlie Bit Me" = 2, "Dramatic Chipmunk" = 3), 
+                  selected = 1),
+      sliderInput("sampleSize", "Please Select Sample Size:", 1, 500, 30),
+     #conditionalPanel(condition = input.Distribution == "Normal",
+                       #textInput("mean", "Please Select the Mean", 5)),
+      #textInput("Standard Deviation", "Select the Standard Deviation")),
+    #conditionalPanel(condition = input.Distribution == "Exponential",
+                     #textInput("lambda", "Select Sample Size: ", min=1, max=500, value=30, step=5),
+    ),
+    mainPanel(
+      plotOutput("plotlyShiny")
     )
   )
 )
-
 server <- function(input, output) {
   output$plotlytable = DT::renderDataTable({
     mtcars
@@ -48,3 +56,4 @@ server <- function(input, output) {
 }
 
 shinyApp(ui, server)
+
